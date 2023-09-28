0d16aed5929a2bbc2e374dc1eb8eb2d3096869fe
crusazer <crusazer@gmail.com>
2023-09-28 10:53:39 +0300

    Changed task_03 of homewwork_04

--- homework_04/task_03.py
+++ homework_04/task_03.py
@@ -4,9 +4,9 @@ i += 1
 
 while i < 101:
     user_answer = input("Should we break?(yes/no): ")
-    if "no" == user_answer:
+    if user_answer == "no":
         break
-    elif "yes" == user_answer:
+    elif user_answer == "yes":
         print(i)
         i += 1
     else:
